c829f2b13ed92bab1957ecceece12250ca454980
Zhenyu Yang <50858833+yzyjsjem@users.noreply.github.com>
2021-11-04 20:33:51 +1100

    complete the navigation bar
    ready to do the game part

---
+++ match game/match game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  match game
+//
+//  Created by 杨振誉 on 2021/11/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ match game/match game/gamepage.swift
@@ -0,0 +1,30 @@
+//
+//  gamepage.swift
+//  match game
+//
+//  Created by 杨振誉 on 2021/11/4.
+//
+
+import UIKit
+
+class gamepage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Match Game"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
